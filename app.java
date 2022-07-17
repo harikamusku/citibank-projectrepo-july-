fe3736378077a4da6ae595c77d910bea054e04d8
harikamusku <harikamusku@gmail.com>
2022-07-17 18:19:12 -0400

    app.java

---
+++ app.java
@@ -0,0 +1,15 @@
+//importing the File class
+import java.io.File;
+
+class Main {
+public static void main(String[] args) {
+
+//[A[A[D[D public static void main(String[] args) {[B[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[A[A[A[A[A[A[3~[3~[3~[3~[3~[3~[3~[3~[3~[C[C[C[C[C[C[C[C[C[C[C[C[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[B[B[B[C[C[C[C[C[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[[A[A[A[A* HelloWorld.java
+ */
+
+public class HelloWorld
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
